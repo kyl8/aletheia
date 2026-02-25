93e6a397fc084153a6dead2e245eef3b1c4ee858
kyl <kyl@kyl.com>
2026-02-24 23:39:54 -0300

    setting up custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aletheia.kyl8.dev',
   server: {
     headers: {
       "Cross-Origin-Opener-Policy": "same-origin",
